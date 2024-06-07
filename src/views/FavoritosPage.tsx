51ee3dd374077db782b48fe98b8bbce27f1af204
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-06-06 22:17:06 -0300

    favoritos

--- src/views/FavoritosPage.tsx
+++ src/views/FavoritosPage.tsx
@@ -1,8 +1,30 @@
+import { useMemo } from "react"
+import DrinkCard from "../components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
 
 export const FavoritosPage = () => {
+
+  const favorites = useAppStore((state) => state.favorites)
+  const hasFavorites = useMemo(() => favorites.length, [favorites])
   return (
     <>
-      <h1>Favoritos</h1>
+      <h1 className="text-6xl font-extrabold  mb-4">Favoritos</h1>
+
+
+      {hasFavorites ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+          {
+            favorites.map(drink => (
+              <DrinkCard
+                key={drink.idDrink}
+                drink={drink}
+              />
+            ))
+          }
+        </div>
+      ) : (
+        <p className="text-2xl text-center">Los favoritos se mostraran aqui</p>
+      )}
     </>
   )
 }
